3c8eaa4cf363368983165e3d5b5ab41845e13584
mori1205 <mm2384mm@gm.tsuda.ac.jp>
2024-12-01 16:36:02 +0900

    速さなどを変更

--- Assets/Scripts/Parameter/ParameterManager.cs
+++ Assets/Scripts/Parameter/ParameterManager.cs
@@ -5,8 +5,8 @@ public class ParameterManager : MonoBehaviour
   // game parameters
   public static float sensitivity = 3.0f;
   public static int limitedTime = 120;
-  public static float maximumSpeed = 15.0f;
-  public static float acceleration = 0.050f;
+  public static float maximumSpeed = 50.0f;
+  public static float acceleration = 1.50f;
   public static float friction = 0.9990f;
 
   // experiment parameters
