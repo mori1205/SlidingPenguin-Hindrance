b25a1f5bb7ff82091f531aba96fbe35454e009cd
mori1205 <mm2384mm@gm.tsuda.ac.jp>
2024-12-04 16:27:38 +0900

    制限時間変更

--- Assets/Scripts/Parameter/ParameterManager.cs
+++ Assets/Scripts/Parameter/ParameterManager.cs
@@ -4,7 +4,7 @@ public class ParameterManager : MonoBehaviour
 {
   // game parameters
   public static float sensitivity = 5.0f; //3
-  public static int limitedTime = 120;
+  public static int limitedTime = 60;
   public static float maximumSpeed = 50.0f;
   public static float acceleration = 1.50f;
   public static float friction = 0.9990f;
